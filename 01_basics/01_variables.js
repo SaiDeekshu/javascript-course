9d50381417be2f0fa79ae0665c425425bfa47d27
Deekshita Cherukuri <154773255+SaiDeekshu@users.noreply.github.com>
2024-07-24 13:46:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+//accountId=2
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+
+/*
+prefer not to use var*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]) 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js Code as newer version//
+//alert(3+3) //we are using nde.js,not browser
+
+console.log(3+3)
+
+console.log("Deekshita")
+
+
+
+let name="hitesh"
+let age=18
+let isloggedIn=false
+let state;
+//number=>2 to power 53
+//bigint
+//string => ""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+    //symbol=>unique
+//object
+console.log(typeof undefined);
+console.log(typeof null);
