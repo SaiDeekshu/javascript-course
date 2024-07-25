854ecfc39415145375f93188a1ba79b22dc39a56
Deekshita Cherukuri <154773255+SaiDeekshu@users.noreply.github.com>
2024-07-25 05:49:56 +0530

    datatype summary

---
+++ 01_basics/05_datatypessummary.js
@@ -0,0 +1,27 @@
+//pritmitive
+//7types:String,number ,boolean,null,undefined,symbol,bigInt
+const score:number=100
+const scoreValue=100.3
+
+const isLogged=false
+const outsideTemp=null
+let userEmail;  //undefined
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anohterId);
+const bigNumber=3456564535n
+
+//references (Non primitive)
+//array,objects,function
+
+const heros=["shakitiman","naagraj","doga"];
+let myObj={
+    name:"Deekshita",
+    age:20,
+}
+const my Function=function(){
+    console.log("Hello World");
+
+}
+console.log(typeof myFunction);
+
